a76e48a1272d7a38ef931a5260c6345af0f45a28
yunus dilber <yunus@initialcode.com.tr>
2025-01-10 16:32:02 +0300

    v1.0.0-beta1

---
+++ Frameworks/LogDropSDK.xcframework/ios-arm64_x86_64-simulator/LogDropSDK.framework/Headers/LogDropSDK.h
@@ -0,0 +1,17 @@
+//
+//  LogDropSDK.h
+//  LogDropSDK
+//
+//  Created by Initial Code on 4.12.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LogDropSDK.
+FOUNDATION_EXPORT double LogDropSDKVersionNumber;
+
+//! Project version string for LogDropSDK.
+FOUNDATION_EXPORT const unsigned char LogDropSDKVersionString[];
+
+
+
