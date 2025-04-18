730fe973a041926c3f74c9297d5a787afcc45fd9
yunus dilber <yunus@initialcode.com.tr>
2025-04-18 12:32:38 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -3,9 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "LogDrop",
-    platforms: [
-        .iOS(.v14)
-    ],
     products: [
         .library(
             name: "LogDropSDK",
